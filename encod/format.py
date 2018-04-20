d7afd7e29ff5224b41c62c1322a58e6b702084e1
Dmitrii Shulyak <x350sh@gmail.com>
2018-04-20 14:52:02 +0300

    update get_list_file() from 3 to 1, make count_word()

--- encod/format.py
+++ encod/format.py
@@ -16,53 +16,37 @@ def recognise_encoging(file):
         return detector.result['encoding']
 
 
-def get_list_text_file(path='.'):
-    list_text_file = []
-    dirs = os.listdir(path)
-    for item in dirs:
-        if '.' in item:
-            if os.path.splitext(item)[1] == '.txt':
-                list_text_file.append(os.path.abspath(item))
-    return list_text_file
-
-
-def get_list_json_file(path='.'):
-    list_json_file = []
-    dirs = os.listdir(path)
-    for item in dirs:
-        if '.' in item:
-            if os.path.splitext(item)[1] == '.json':
-                list_json_file.append(os.path.abspath(item))
-    return list_json_file
-
-
-def get_list_xml_file(path='.'):
+def get_list_file(path='.', extend='.txt'):
     list_xml_file = []
     dirs = os.listdir(path)
     for item in dirs:
         if '.' in item:
-            if os.path.splitext(item)[1] == '.xml':
+            if os.path.splitext(item)[1] == extend:
                 list_xml_file.append(os.path.abspath(item))
     return list_xml_file
 
 
-def count_max_occurr_word(file):
-    word_count = {}
+def text_file_to_list_word(file):
     encod = recognise_encoging(file)
     with open(file, 'r', encoding=encod) as f:
         data = f.read().lower().split()
-        for i in data:
-            if (i in data) and (len(i) > 6):
-                if i in word_count:
-                    word_count[i] += 1
-                else:
-                    word_count[i] = 1
+        return data
+
+
+def count_word(data):
+    word_count = {}
+    for i in data:
+        if (i in data) and (len(i) > 6):
+            if i in word_count:
+                word_count[i] += 1
+            else:
+                word_count[i] = 1
     word_count_tuple = word_count.items()
     word_count_result = sorted(word_count_tuple, key=operator.itemgetter(1), reverse=True)
     print(word_count_result[:10])
 
 
-def count_word_in_json(file):
+def json_file_to_list_word(file):
     all_str = ''
     word_count = {}
     encod = recognise_encoging(file)
@@ -75,27 +59,16 @@ def count_word_in_json(file):
             all_str += item['title']
             all_str += item['description']
     list_str = all_str.lower().split()
-    for i in list_str:
-        if (i in list_str) and (len(i) > 6):
-            if i in word_count:
-                word_count[i] += 1
-            else:
-                word_count[i] = 1
-    word_count_tuple = word_count.items()
-    word_count_result = sorted(word_count_tuple, key=operator.itemgetter(1), reverse=True)
-    print(word_count_result[:10])
-        
+    return list_str
 
 
 if __name__ == "__main__":
-   list_text_file = get_list_text_file()
+   list_text_file = get_list_file()
    for item in list_text_file:
        print(f"Частота слов в файле {os.path.basename(item)}:")
-       count_max_occurr_word(item)
+       count_word(text_file_to_list_word(item))
 
-   list_json_file = get_list_json_file()
+   list_json_file = get_list_file(extend='.json')
    for item in list_json_file:
        print(f"Частота слов в файле {os.path.basename(item)}:")
-       count_word_in_json(item)
-
-# count_word_in_json('newsafr.json')
+       count_word(json_file_to_list_word(item))
