af7f66d6dea913507bdddaa62a5afa2226617b8a
Dmitrii Shulyak <x350sh@gmail.com>
2018-04-28 18:25:45 +0300

    conv.py with support Linux

--- lesson2.5/conv.py
+++ lesson2.5/conv.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-def detect_patform():
+def detect_platform():
 	return sys.platform
 
 def detect_content_dir(name_dir, name_curent_dir):
@@ -14,8 +14,22 @@ def detect_content_dir(name_dir, name_curent_dir):
 			return os.path.join(name_curent_dir, name_dir)
 
 
+def detect_content_dir_linux(name_dir, name_curent_dir):
+	my_dir = subprocess.Popen(['ls', name_curent_dir], stdout=subprocess.PIPE, shell=True)
+	for line in my_dir.stdout:
+		if line.decode('utf-8').strip() == name_dir:
+			return os.path.join(name_curent_dir, name_dir)
+
+
+
+
 def make_dir(name_new_dir):
-	subprocess.Popen(['mkdir', name_new_dir], shell=True)
+	subprocess.Popen(['mkdir ', name_new_dir], shell=True)
+	return os.path.join(os.getcwd(), name_new_dir)
+
+
+def make_dir_linux(name_new_dir):
+	subprocess.Popen(['mkdir ' + name_new_dir], shell=True)
 	return os.path.join(os.getcwd(), name_new_dir)
 
 
@@ -29,7 +43,23 @@ def run_programm(programm):
 # 	command[5] = os.path.join(path, name_file)
 # 	return command[1:]
 
-def make_commands():
+def make_commands_linux(command):
+	dir_sourse = input("Input source dir: ")
+	size = input("Input size in px: ")
+	dir_result = input("Input result dir: ")
+	new_path = None
+	new_path = detect_content_dir_linux(dir_result, os.getcwd())
+	if not new_path:
+		new_path = make_dir_linux(dir_result)
+	list_dir = os.listdir(dir_sourse)
+	result = []
+	if list_dir:
+		for item in list_dir:
+			result.append([command, os.path.join(dir_sourse, item), '-resize', size, os.path.join(dir_result, item)])
+	return result	
+
+
+def make_commands(command):
 	dir_sourse = input("Input source dir: ")
 	size = input("Input size in px: ")
 	dir_result = input("Input result dir: ")
@@ -41,8 +71,10 @@ def make_commands():
 	result = []
 	if list_dir:
 		for item in list_dir:
-			result.append(['convert.exe', os.path.join(dir_sourse, item), '-resize', size, os.path.join(dir_result, item)])
+			result.append([ command, os.path.join(dir_sourse, item), '-resize', size, os.path.join(dir_result, item)])
 	return result	
+
+
 
 
 
@@ -56,5 +88,13 @@ def make_commands():
 
 # run_programm(make_command_with_output_dir(command, new_path))
 
-for i in make_commands():
-	run_programm(i)
+if  detect_platform() == 'win32':
+	for i in make_commands('convert.exe'):
+		run_programm(i)
+elif detect_platform() == 'linux':
+	print("Linux")
+	for i in make_commands_linux('convert'):
+		run_programm(i)
+
+
+#/usr/lib/x86_64-linux-gnu/ImageMagick-6.8.9/bin-Q16
