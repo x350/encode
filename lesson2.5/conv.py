a245a17486e44708046325106a44119db8792ee1
Dmitrii Shulyak <x350sh@gmail.com>
2018-04-29 18:40:24 +0300

    Support Linux and Windows

--- lesson2.5/conv.py
+++ lesson2.5/conv.py
@@ -1,35 +1,38 @@
 import os
 import sys
 import subprocess
+from multiprocessing import Process
+from queue import Queue
+
 
 def detect_platform():
-	return sys.platform
+    return sys.platform
+
 
 def detect_content_dir(name_dir, name_curent_dir):
-	if detect_platform() == 'win32':
-		my_dir = subprocess.Popen(['dir', name_curent_dir], stdout=subprocess.PIPE, shell=True, encoding='cp866')
-		for line in my_dir.stdout:
-			if len(line) < 3:
-				continue
-			if line.split()[2] == '<DIR>' and line.split()[3] == name_dir:
-				return os.path.join(name_curent_dir, name_dir)
-	elif detect_platform() == 'linux':
-		my_dir = subprocess.Popen(['ls', name_curent_dir], stdout=subprocess.PIPE, shell=True)
-		for line in my_dir.stdout:
-			if line.decode('utf-8').strip() == name_dir:
-				return os.path.join(name_curent_dir, name_dir)
+    if detect_platform() == 'win32':
+        my_dir = subprocess.Popen(['dir', name_curent_dir], stdout=subprocess.PIPE, shell=True, encoding='cp866')
+        for line in my_dir.stdout:
+            if len(line) < 3:
+                continue
+            if line.split()[2] == '<DIR>' and line.split()[3] == name_dir:
+                return os.path.join(name_curent_dir, name_dir)
+    elif detect_platform() == 'linux':
+        my_dir = subprocess.Popen(['ls', name_curent_dir], stdout=subprocess.PIPE, shell=True)
+        for line in my_dir.stdout:
+            if line.decode('utf-8').strip() == name_dir:
+                return os.path.join(name_curent_dir, name_dir)
 
 
 
 def make_dir(name_new_dir):
-	subprocess.Popen(['mkdir', name_new_dir], shell=True)
-	return os.path.join(os.getcwd(), name_new_dir)
-
-
-def make_dir_linux(name_new_dir):
-	subprocess.Popen(['mkdir ' + name_new_dir], shell=True)
-	return os.path.join(os.getcwd(), name_new_dir)
-
+    if detect_platform == 'win32':
+        args = ['mkdir', name_new_dir]
+    elif detect_platform == 'linux':
+        args = ['mkdir ' + name_new_dir]
+    subprocess.Popen(['mkdir', name_new_dir], shell=True)
+    return os.path.join(os.getcwd(), name_new_dir)
+    
 
 def run_programm(programm):	
 	subprocess.Popen(programm, stdout=subprocess.PIPE)
@@ -61,8 +64,21 @@ elif detect_platform() == 'linux':
 else:
 	print("Unknown platform")
 
-
 for i in make_commands(command):
-	run_programm(i)
+    run_programm(i)
+
+
+# p = Process(target = f, args =("Bob",))
+#q = Queue()
+# p = Process(target=run_programm, args=(i))
+#for i in make_commands(command):
+#    q.put(i)
+
+#for i in range(4):
+#    print('Process ', i)
+#    p = Process(target=run_programm, args=(q.get()))
+#    p.start()
+
+#q.join()
 
 
