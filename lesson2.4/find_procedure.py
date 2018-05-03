56859931b5ea17c7ec8252f638503e5aa71b6034
Dmitrii Shulyak <x350sh@gmail.com>
2018-05-03 20:47:59 +0300

    correction programm, find_procedure.py, according to the teacher's comments

--- lesson2.4/find_procedure.py
+++ lesson2.4/find_procedure.py
@@ -16,11 +16,10 @@ def recognise_encoging(file):
 
 def get_list_file(dir_file, extend='.sql'):
     list_file = []
-    dirs = os.listdir(os.path.join(current_dir, dir_file))
-    for item in dirs:
-        if '.' in item:
-            if os.path.splitext(item)[1] == extend:
-                list_file.append(os.path.join(current_dir, dir_file, item))
+    files_in_dir = os.listdir(os.path.join(current_dir, dir_file))
+    for item in files_in_dir:
+        if os.path.splitext(item)[1] == extend:
+            list_file.append(os.path.join(current_dir, dir_file, item))
     return list_file
 
 
@@ -36,15 +35,13 @@ def found_substring_in_file(file, substring):
 
 
 def check_list(list_file, my_substring):
-    count = 0
     result_list = []
     for item in list_file:
         i = found_substring_in_file(item, my_substring)
         if i:
             result_list.append(i)
             print(i)
-            count += 1
-    print(f'Total: {count}')
+    print(f'Total: {len(result_list)}')
     return result_list
 
 if __name__ == '__main__':
@@ -52,9 +49,9 @@ if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
     list_found_file = get_list_file(dir_file=dir_with_files)
     while True:
-        my1_substring = input_substring()
-        if my1_substring == 'q':
+        user_request = input_substring()
+        if user_request == 'q':
             break
-        list_found_file = check_list(list_found_file, my1_substring)
+        list_found_file = check_list(list_found_file, user_request)
         if len(list_found_file) <= 1:
             break
