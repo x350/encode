a12d338569ad9835cad813a06430803a93981849
Dmitrii Shulyak <x350sh@gmail.com>
2018-05-15 23:30:31 +0300

    creating list ID with page, for vk_client.py

--- lesson3.3/vk_client.py
+++ lesson3.3/vk_client.py
@@ -21,17 +21,21 @@ olga_id = 488003955
 TOKEN = '21c43c91e3e5137074f7ccfc737f316e8e3217d8f3457c329021ad34b3b6be2efba8bf092e450e43cb51b'
 
 
-def find_common_friends(source_id, target_id, token):
+def find_common_friends(source_id, friend_id, token):
     response = requests.get(
         'https://api.vk.com/method/friends.getMutual',
-        params=dict(v='5.74', access_token=token, source_uid=source_id, target_uid=target_id)
+        params=dict(v='5.74', access_token=token, source_uid=source_id, target_uid=friend_id)
     ).json()
     resp = response.get('response', [])
+    page_list = []
     for item in resp:
-        print('For User_ID = {} -> page: {}'.format(item, ''.join(('https://vk.com/id', str(item)))))
+        page = ''.join(('https://vk.com/id', str(item)))
+        # print('For User_ID = {} -> page: {}'.format(item, page))
+        page_list.append(page)
+    return list(zip(resp, page_list))
 
 
 if __name__ == '__main__':
-    find_common_friends(my_id, target_id, TOKEN)
+    print(find_common_friends(my_id, target_id, TOKEN))
 
 
